9870288f60e0034638c10c9fb6ae58e626cb53c5
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-25 17:33:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,3 +10,11 @@ def reverse_each_word(r_words)
 end
 
 reverse_each_word(greeting)
+def reverse_each_word(r_words)
+  first_array = r_words.split(" ")
+  fin_array = []
+  first_array.collect do |r_words|
+    fin_array << r_words.reverse
+  end
+  fin_array.join(" ")
+end
